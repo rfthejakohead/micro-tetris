a69bbfc9ad7aa3fd2f43fd6164fc8a8939eb5d7d
rfthejakohead <rfthejakohead@hotmail.com>
2017-10-22 19:40:19 +0100

    Removed license p.2

--- screen.hpp
+++ screen.hpp
@@ -4,25 +4,6 @@
 #include <vector>
 #include <string>
 
-/*
-Copyright Rafael 'rfthejakohead' Fernandes 2016
-
-This file is part of micro:tetris.
-
-micro:tetris is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-micro:tetris is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with micro:tetris.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 class Screen {
     vector < vector < unsigned char > > bufferData; // Pixels in screen
     MicroBitDisplay displayObj; // Display object
